47d657df2c668c71643cc921c04e949efaaca151
Athithiyan.M.R <88970267+Athithiyanmr@users.noreply.github.com>
2026-01-28 00:06:25 +0530

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,7 +6,7 @@ ROOT = Path(__file__).resolve().parents[1]
 sys.path.append(str(ROOT))
 
 from fastapi.testclient import TestClient
-from geo_crs_converter.main import app
+from backend.app.main import app
 
 client = TestClient(app)
 
